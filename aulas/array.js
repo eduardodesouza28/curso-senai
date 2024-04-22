df818928626b511f6c3c59f91d0e5ab5bdb61f61
EDUARDO DE SOUZA <eduardo_souza28@estudante.sesisenai.org.br>
2024-04-22 17:08:10 -0300

    array (vetor)

---
+++ aulas/array.js
@@ -0,0 +1,9 @@
+let nu = [1, 2, 4, 8, 7]
+// nu.push(5) adiciona no final do array
+// nu.unshift(3) adiciona no inicio do array
+// nu.pop() remove o ultimo item do array
+// nu.shift() remove o primeiro item do array
+
+for(let i=0; i<nu.length; i++){
+    console.log(nu[i]);
+}
---
+++ aulas/ex3.js
@@ -0,0 +1,8 @@
+let numeros = []
+
+
+for(let i = 0; i < 3; i++ ){
+    numeros[i] = prompt("digite o número")
+}
+numeros.reverse()
+alert(numeros)
