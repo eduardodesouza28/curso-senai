f5865d209d2cff68b3e84d84a05e325e0a6b462d
EDUARDO DE SOUZA <eduardo_souza28@estudante.sesisenai.org.br>
2024-04-25 14:13:14 -0300

    organização (só)

--- aulas/ex3.js
+++
@@ -1,8 +0,0 @@
-let numeros = []
-
-
-for(let i = 0; i < 3; i++ ){
-    numeros[i] = prompt("digite o número")
-}
-numeros.reverse()
-alert(numeros)
--- aulas/script.js
+++ aulas/script.js
@@ -1,9 +1,4 @@
-function redirect() {
-    window.location.href = "https://www.bing.com/search?q=window.location.href+&qs=n&form=QBRE&sp=-1&lq=0&sm=csrmain&pq=window.location.href+&sc=11-21&sk=&cvid=36F77AF52C18427B83E83124A16553FB&ghsh=0&ghacc=0&ghpl="
-}
-function adElemento() {
-    document.getElementById("titulo").innerHTML = "teste"
-}
+
 function ad(a) {
     document.getElementById("display").value += a
 }
@@ -14,19 +9,13 @@ function resultado() {
 function limpar() {
     document.getElementById("display").value = ""
 }
-function lampada() {
-    function ligar() {
-        let on = document.getElementById("off")
-        on.src = "on.gif"
-        document.body.appendChild(on)
-    }
-    function desligar() {
-        let off = document.getElementById("off")
-        off.src = "off.gif"
-        // document.body.removeChild(on)
-        document.body.appendChild(on)
-
-    }
+function ligar() {
+    let on = document.getElementById("off")
+    on.src = "on.gif"
+}
+function desligar() {
+    let off = document.getElementById("off")
+    off.src = "off.gif"
 }
 function mostrar() {
 
@@ -43,10 +32,10 @@ function mostrar() {
 
 
 }
-function compra() { 
+function compra() {
     let quantidade = Number(prompt("quantos você dejesa comprar?"))
-    if (quantidade > 0 && quantidade < 30){
-        alert (500 * quantidade)
+    if (quantidade > 0 && quantidade < 30) {
+        alert(500 * quantidade)
         document.getElementById("estq").innerHTML -= quantidade
     } else {
         alert("quantidade indisponivel")
